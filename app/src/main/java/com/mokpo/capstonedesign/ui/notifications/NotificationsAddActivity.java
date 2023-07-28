50d99606e2d36d672b4298a8554eb01ba4a49e59
SungJin99 <wls0036@gmail.com>
2023-07-28 15:56:57 +0900

    ui 정적 -> 동적 변경

---
+++ app/src/main/java/com/mokpo/capstonedesign/ui/notifications/NotificationsAddActivity.java
@@ -0,0 +1,44 @@
+package com.mokpo.capstonedesign.ui.notifications;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+import com.mokpo.capstonedesign.R;
+
+public class NotificationsAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications_add);
+
+        Button regButton = findViewById(R.id.reg_button);
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add your code for handling the button click here
+                // For example, you can retrieve the entered title and content from the EditText views
+                EditText titleEditText = findViewById(R.id.title_et);
+                String title = titleEditText.getText().toString();
+
+                EditText contentEditText = findViewById(R.id.content_et);
+                String content = contentEditText.getText().toString();
+
+                // Perform any necessary actions with the entered title and content
+                // For now, let's just display them in logs
+                Log.d("NotificationsAddActivity", "Title: " + title);
+                Log.d("NotificationsAddActivity", "Content: " + content);
+
+                // You can also navigate back to the previous fragment or finish the activity here
+                finish();
+            }
+        });
+
+
+        // Add your code for handling the activity here
+        // For example, you can initialize views and set up any necessary event listeners
+    }
+}
--- app/src/main/java/com/mokpo/capstonedesign/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/mokpo/capstonedesign/ui/notifications/NotificationsFragment.java
@@ -1,37 +1,45 @@
 package com.mokpo.capstonedesign.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
+import android.widget.Button;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.mokpo.capstonedesign.R;
 import com.mokpo.capstonedesign.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
-
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button regButton = view.findViewById(R.id.reg_button);
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Execute code to navigate to NotificationsAddActivity
+                Intent intent = new Intent(getActivity(), NotificationsAddActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
+
+
+
