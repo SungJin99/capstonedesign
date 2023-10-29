e85fc3365ba0cafa646f305123306d19170932a1
SungJin99 <wls0036@gmail.com>
2023-10-30 01:51:21 +0900

    댓글 수정페이지 추가

---
+++ app/src/main/java/com/mokpo/capstonedesign/ui/community/CommentUpdate.java
@@ -0,0 +1,17 @@
+package com.mokpo.capstonedesign.ui.community;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mokpo.capstonedesign.R;
+
+public class CommentUpdate extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community_comment_update);
+
+    }
+}
